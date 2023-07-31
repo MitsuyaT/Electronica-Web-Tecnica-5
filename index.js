3f431aaa6e860425cca3fb2a689422f6907ddbf4
Alan <elpinguinoalan@gmail.com>
2023-07-31 02:13:57 -0300

    Dark && Light Mode

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+//! Funcionalidad de
+//! la Barra de Navegación
+
 let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sideNavBar");
+let sideBar = document.querySelector(".sideNavBar");
 let iconoColegio = document.querySelector("#icon");
 let tituloColegio = document.querySelector("#topH2");
+let mainInside = document.querySelector(".mainInside");
+
 const listaNav = document.querySelectorAll("#list");
 
 
 btn.onclick = function() {
-    sidebar.classList.toggle("active");
+    sideBar.classList.toggle("active");
     iconoColegio.classList.toggle("active");
     tituloColegio.classList.toggle("active");
+    mainInside.classList.toggle("active");
    
     for(let index=0; index<listaNav; index++) {
         listaNav[index].classList.toggle("active");
